25a0568a1446a16982ad9ae3c903ac6b40c3cdeb
slowisfast2030 <slowisfast2030@163.com>
2023-02-24 18:47:02 +0800

    all is well

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,12 @@ var calledLibrary = artifacts.require("calledLibrary");
 
 module.exports = function(deployer) {
 
-  deployer.deploy(calledContract);
-  deployer.deploy(calledLibrary);
+  // deployer.deploy(calledContract);
+  // deployer.deploy(calledLibrary);
 
-  deployer.link(calledLibrary, [caller]);
+  // deployer.link(calledLibrary, [caller]);
 
   deployer.deploy(caller);
 };
+
+// 惊人发现，如果注释掉3行，不影响
